580a191fc3eb51a8c5e056980dd31b155c3781ff
Yaroslav Fedevych <jafd@netstyle.com.ua>
2012-01-02 11:56:34 +0200

    Made queue poll timeout configurable; Complete logic overhaul towards more sane behaviour (per-runner queue)

--- txfileio.py
+++ txfileio.py
@@ -1,55 +1,97 @@
-from twisted.internet import defer
+from twisted.internet import defer, threads
+from twisted.python.failure import Failure
+import Queue
+import sys
+import time
 
 class FileIOProxy(object):
     """
-    A FileIOProxy proxies calls to file operations, so they all return Deferreds.
+    A FileIOProxy proxies calls to file operations, so they all return Deferreds; the operations
+    themselves are queued up and delegated to worker threads. Deferreds fire when the workers have
+    finished the operations.
     """
     def __init__(self, manager, fd):
-        pass
+        self.manager = manager
+        self.fd = fd
 
     def _proxyfunc(self, op):
-        def _inner(self, *args, **kwargs):
-            return self.manager.enqueue(self, op, args, kwargs)
+        def _inner(*args, **kwargs):
+            return self.manager.enqueue(op, self, args, kwargs)
         fn = getattr(self.fd, op)
         _inner.__name__ = fn.__name__
         _inner.__doc__ = fn.__doc__
-        _inner.__dict__.update(fn.__dict__)
+        if hasattr(fn, '__dict__'):
+            _inner.__dict__.update(fn.__dict__)
         return _inner
 
     def __getattr__(self, attrname):
-        if hasattr(self.fd, attrname) and callable(getattr(self.fd, attrname)):
-            return self._proxyfunc(attrname)
-        return object.__getattr__(attrname)
+        if hasattr(self.fd, attrname):
+            if callable(getattr(self.fd, attrname)):
+                return self._proxyfunc(attrname)
+            return getattr(self.fd, attrname)
+        raise AttributeError("The object of class '{0}' has no attribute '{1}'".format(self.fd.__class__.__name__, attrname))
 
+class Operation(object):
+    __slots__ = ('fd', 'name', 'deferred', 'args', 'kwargs')
+    def __init__(self, **kwargs):
+        self.args = kwargs.get('args', None)
+        self.kwargs = kwargs.get('kwargs', None)
+        for i in ('fd', 'name', 'deferred'):
+            setattr(self, i, kwargs.get(i))
+            
+    def __str__(self):
+        return "File operation: {0}(*{1}, **{2})".format(self.name, repr(self.args), repr(self.kwargs))
+
+class Runner(object):
+    __slots__ = ('fd', 'busy', 'running', 'queue', 'manager')
+    def __init__(self, manager):
+        self.queue = Queue.Queue()
+        self.fd = None
+        self.busy = False
+        self.running = False
+        self.manager = manager
+
+    def enqueue(self, op):
+        self.queue.put(op)
+        return op.deferred
+        
+    def __call__(self):
+        self.running = True
+        while self.running:
+            try:
+                op = self.queue.get(True, self.manager.queue_timeout)
+                self.fd = op.fd
+                self.busy = True
+                if (op.fd is None) and (op.name != 'open'):
+                    raise RuntimeError("Calling a file operation {0} on None".format(op.name))
+                if op.name == 'open':
+                    result = self.manager.take(open(*op.args, **op.kwargs))
+                    op.fd = result
+                else:
+                    result = getattr(op.fd.fd, op.name)(*op.args, **op.kwargs)
+                threads.blockingCallFromThread(self.manager.reactor, op.deferred.callback, result)
+            except Queue.Empty:
+                pass
+        return True
 
 class FileIOManager(object):
     """
-    A process which runs in a separate thread and does actually take care of I/O
-    operations. It processes requests to open files, to read, write and seek in them.
+    This class is used to process file I/O. It spawns runners into separate threads,
+    and those runners fetch I/O operations from the I/O queue.
     """
-    def __init__(self, reactor, minrunners=2):
+    def __init__(self, reactor, maxrunners=5, queue_timeout=1):
         self.reactor = reactor
-        self.opqueue = self.buildOpQueue()
-        self.resultqueue = self.buildResultQueue()
         self.accept_operations = True
-        self.running = False
+        self.queue_timeout = queue_timeout
         self.runners = []
+        self.runner_no = 0
         self.stopdeferred = defer.Deferred()
         self.maxrunners = maxrunners
+        for i in range(self.maxrunners - 1):
+            self.spawnRunner()
 
     def buildOpQueue(self):
         return Queue.Queue()
-
-    def buildResultQueue(self):
-        return Queue.Queue()
-
-    def open(self, filename, mode="r", buffering=None):
-        """
-        Open a file. Return a Deferred which results in a filehandle proxy.
-        The filehandle proxy is almost entirely like a file-like object, except
-        t
-        """
-        pass
 
     def take(self, fileobject):
         """
@@ -58,57 +100,28 @@ class FileIOManager(object):
         fp = FileIOProxy(self, fileobject)
         return fp
 
-    def enqueue(self, fd, op, args, kwargs):
+    def enqueue(self, op, fd, args, kwargs):
         if not self.accept_operations:
             raise RuntimeError("Operations queued past service stop")
-        # Clean up stopped runners
-        self.runners = filter(lambda r: not r['stopped'], self.runners)
-        # Find if we have non-busy runners
-        ready = len(filter(lambda r: not r['busy'], self.runners)) > 0
-        if not ready and len(self.runners) < self.maxrunners:
-            self.spawnRunner()
         d = defer.Deferred()
-        self.opqueue.put({
-            'op' : op,
-            'args' : args,
-            'kwargs' : kwargs,
-            'deferred' : d,
-            'fd' : fd
-            })
+        operation = Operation(name=op, args=args, kwargs=kwargs, deferred=d, fd=fd)
+        filtered = filter(lambda x: x.fd is fd, self.runners)
+        if len(filtered):
+            filtered[0].enqueue(operation)
+        else:
+            self.runner_no = (self.runner_no + 1) % len(self.runners)
+            self.runners[self.runner_no].enqueue(operation)
         return d
 
     def spawnRunner(self):
-        def _runner(self, record):
-            timeouts = 0
-            while record['running']:
-                try:
-                    op = self.opqueue.get(block=True, timeout=1)
-                    record['busy'] = True
-                    record['currentfd'] = op['fd']
-                    try:
-                        if op['name'] == 'open':
-                            result = open(*op['args'], **op['kwargs'])
-                        else:
-                            result = getattr(op['fd'], op['name']).__call__(*op['args'], **op['kwargs'])
-                        self.reactor.callFromThread(op['deferred'].callBack, result)
-                    except Exception as e:
-                        pass
-                except Queue.Empty:
-                    timeouts += 1
-                finally:
-                    if timeouts > 10:
-                        record['running'] = False
-                return True
-
-        def _recstop(self, record, result):
-            self.runners[record]['stopped'] = True
-            return result
-
-        record = { 'running' : True, 'busy' : False, 'stopped' : False, 'currentfd' : None }
-        self.runners.append(record)
-        record['stopDeferred'] = self.reactor.deferToThread(_runner, self, record).addCallback(_recstop, self, record)
+        r = Runner(self)
+        self.runners.append(r)
+        return threads.deferToThread(r).addCallback(lambda _: self.runners.remove(r))
 
     def stop(self):
         self.accept_operations = False
-        self.running = False
-        pass
+        for x in self.runners:
+            x.running = False
+        
+    def open(self, *args, **kwargs):
+        return self.enqueue('open', None, args, kwargs)
