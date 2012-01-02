c8374b666521136291f054862d539ddbe4519634
Yaroslav Fedevych <jafd@netstyle.com.ua>
2012-01-02 15:41:22 +0200

    setup.py using setuptools and falling back to distutils

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='txfileio',
+      version='0.0.0',
+      description='Asynchronous file I/O for Twisted',
+      author='Yaroslav Fedevych',
+      author_email='jaroslaw.fedewicz@gmail.com',
+      url='https://github.com/jafd/txfileio',
+      py_modules=['txfileio'],
+     )
+
